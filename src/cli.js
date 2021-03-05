515447dd13ac1ae0a6f14926cab3f0792ef9bf88
Jason Gardner <jason.gardner.lv@gmail.com>
2021-03-04 16:18:38 -0800

    First commit

---
+++ src/cli.js
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+import chalk from 'chalk'
+import commandLineArgs from 'command-line-args'
+import commandLineUsage from 'command-line-usage'
+import fs from 'fs'
+import glob from 'glob'
+
+import {findJsonDuplicatesFromFiles} from './index'
+
+const defOption = [
+  {
+    name: 'fileglob',
+    type: String,
+    defaultOption: true,
+    typeLabel: '{underline glob}',
+    description: 'The glob path to the JSON files. ex. path/to/the/**/*-files.json'
+  }
+];
+
+const sections = [
+  {
+    header: 'Find duplicated keys or values in json files.',
+    content: 'List all duplicates (including nested) keys or values across JSON files, using exact or fuzzy matching.\n\nExample:\nnode ./build/cli path/to/the/**/*-files.json'
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        alias: 'v',
+        name: 'values',
+        type: Boolean,
+        typeLabel: '{underline boolean}',
+        description: 'Find duplicate values instead of keys'
+      },
+      {
+        alias: 't',
+        name: 'threshold',
+        type: Number,
+        defaultValue: 0.0,
+        typeLabel: '{underline number}',
+        description: 'Fuzzy threshold, 0.0 - 1.0. Default is 0.0, which is a (case-insensitive) substring match. See https://fusejs.io/api/options.html#threshold for more information.'
+      },
+      {
+        alias: 'h',
+        name: 'help',
+        type: Boolean,
+        typeLabel: '{underline boolean}',
+        description: 'Show this help.'
+      }
+    ]
+  }
+]
+
+const options = commandLineArgs(defOption.concat(sections[1].optionList));
+
+const runCli = (options) => {
+  if (options == null || options.fileglob == null) {
+    console.log(commandLineUsage(sections));
+    return 0;
+  } else {
+    const jsonFilePath = options.fileglob;
+    try {
+      const jsonFiles = glob.sync(jsonFilePath, {});
+      if (!jsonFiles || !jsonFiles.length) {
+        console.log(`No JSON files found.`);
+        return 0;
+      }
+      const jsonArray = [];
+      let parsed, parseErr;
+      jsonFiles.every(f => {
+        try {
+          parsed = JSON.parse(fs.readFileSync(f));
+          jsonArray.push({filename: f, json: parsed});
+          return true;
+        } catch (err) {
+          parseErr = true;
+          console.log(chalk.red(`Unable to read/parse JSON file: ${f}`));
+          console.log(chalk.red(err));
+          return false;
+        }
+      });
+      if (!!parseErr) return 1;
+      console.log('');
+      console.log(`Processing ${jsonArray.length} JSON files(s)`);
+      console.log('');
+      findJsonDuplicatesFromFiles(jsonArray, options);
+    } catch (err) {
+      console.log(chalk.red(err));
+      return 1;
+    }
+  }
+}
+
+process.exit(runCli(options))
---
+++ src/index.js
@@ -0,0 +1,98 @@
+import chalk from 'chalk';
+import Fuse from 'fuse.js';
+
+export function findJsonDuplicatesFromFiles(jsonArray, options) {
+  const dict = findJsonDuplicates(jsonArray, options);
+  generateOutput(dict, options);
+}
+
+export function findJsonDuplicates(jsonArray, options) {
+  const dict = {};
+  jsonArray.forEach(file => {
+    if (!!options.values)
+      mapPropertyValues(options, dict, file.json, file.filename);
+    else
+      mapPropertyKeys(options, dict, file.json, file.filename);
+  });
+  return dict;
+}
+
+const mapPropertyKeys = (options, dict, json, filename, path) => {
+  let newPath;
+  Object.keys(json).forEach(k => {
+    newPath = !!path ? path + '.' + k : k;
+    if (!dict.hasOwnProperty(k)) dict[k] = [];
+    if (Array.isArray(json[k])) {
+      //TODO handle array
+    } else if (typeof json[k] === 'object' && json[k] !== null) {
+      mapPropertyKeys(options, dict, json[k], filename, newPath);
+    } else {
+      // if (options.threshold === undefined) {
+      //   dict[k].push({value: json[k], keyPath: newPath, file: filename});
+      // } else {
+        const fuse = new Fuse(Object.keys(dict), {includeScore:true, threshold: options.threshold});
+        fuse.search(k).forEach(r => {
+          if (Math.abs(k.length - r.item.length) <= 3 && (options.threshold - r.score >= 0))
+            dict[r.item].push({key: k, value: json[k], keyPath: newPath, file: filename, score: r.score});
+        });
+      // }
+    }
+  })
+}
+
+const mapPropertyValues = (options, dict, json, filename, path) => {
+  let newPath;
+  Object.keys(json).forEach(k => {
+    newPath = !!path ? path + '.' + k : k;
+    if (!dict.hasOwnProperty(json[k])) dict[json[k]] = [];
+    if (Array.isArray(json[k])) {
+      //TODO handle array
+    } else if (typeof json[k] === 'object' && json[k] !== null) {
+      mapPropertyValues(options, dict, json[k], filename, newPath);
+    } else {
+      // if (options.threshold === undefined) {
+      //   dict[json[k]].push({key: k, keyPath: k, file: filename});
+      // } else {
+        const fuse = new Fuse(Object.keys(dict), {includeScore:true, threshold: options.threshold});
+        fuse.search(json[k]).forEach(r => {
+          if (Math.abs(json[k].length - r.item.length) <= 3 && (options.threshold - r.score >= 0))
+            dict[r.item].push({key: k, value: json[k],keyPath: newPath, file: filename, score: r.score});
+        });
+      // }
+    }
+  })
+}
+
+const generateOutput = (dict, options) => {
+  let numDupes = 0;
+  Object.keys(dict).forEach(k => {
+    if (dict[k].length > 1) {
+      ++numDupes;
+      console.log('----------------------------------------');
+      console.log('');
+      if (!!options.values)
+        console.log(chalk.yellow(chalk.gray('Value:') + ` ${k}`));
+      else
+        console.log(chalk.yellow(chalk.gray('Key:') + ` ${k}`));
+      dict[k].forEach(m => {
+        console.log('');
+        if (!!m.key) console.log(chalk.gray('Key:') + ` ${m.key}`);
+        if (!!m.value) console.log(chalk.gray('Value:') + ` ${m.value}`);
+        if (!!m.keyPath && m.keyPath !== m.key) console.log(chalk.gray('Path:') + `  ${m.keyPath}`);
+        if (!!m.file) console.log(chalk.gray('File:') + `  ${m.file}`);
+        if (!!m.score) console.log(chalk.gray('Score:') + `  ${m.score}`);
+      });
+      console.log('');
+    }
+  })
+  console.log('----------------------------------------');
+  console.log('');
+  console.log('✨ Done');
+  console.log('');
+  if (numDupes === 0) {
+    console.log(chalk.green('No duplicates found 🎉'));
+    return;
+  } else {
+    console.log(chalk.yellow(`${numDupes} duplicates found!`));
+  }
+}
